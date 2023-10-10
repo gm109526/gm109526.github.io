7639dee34b5c8c112de10797084cf3e10deee42f
gm109526 <matthieu.garcia-gimenez@etu.unice.fr>
2023-10-10 09:30:43 +0200

    modif path

--- CartoTD3/2/js/main.js
+++ CartoTD3/2/js/main.js
@@ -25,7 +25,7 @@ camera.position.z = 5;
 
 const loader = new GLTFLoader();
 
-loader.load('mr_krabs_spongebob.glb', function (gltf) {
+loader.load('./mr_krabs_spongebob.glb', function (gltf) {
     const model = gltf.scene.children[0];
     model.scale.set(0.1, 0.1, 0.1);
     scene.add(gltf.scene);
